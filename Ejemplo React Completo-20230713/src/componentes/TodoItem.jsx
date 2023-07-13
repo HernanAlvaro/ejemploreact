7fd2b3a4655411650cd595acafdbeec5b330dc71
Alvaro Uviña <129318190+HernanAlvaro@users.noreply.github.com>
2023-07-13 19:41:43 -0400

    Add files via upload

---
+++ Ejemplo React Completo-20230713/src/componentes/TodoItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export function TodoItem({todo,cambiarEstado}){
+    const {id,task,complete}=todo
+    const fnCambiarEstado=()=>{
+        cambiarEstado(id);
+    }
+    return (
+        <>
+            <li className="list-group-item">
+            <input className="form-check-input me-2" onChange={fnCambiarEstado} checked={complete} 
+            type="checkbox">
+            </input>
+            {task}
+            </li>
+        </>
+    );
+}
